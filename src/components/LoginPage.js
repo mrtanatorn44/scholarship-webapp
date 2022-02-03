31676d430defc8ffc46c81f05297f10c5cdbecc3
petchy95 <mrtanatorn44@gmail.com>
2022-02-03 16:51:19 +0700

    create Login page

---
+++ src/components/LoginPage.js
@@ -0,0 +1,20 @@
+import './LoginPage.css';
+
+function LoginPage() {
+  return (
+
+    <body>
+      <div class="d-flex">
+        <div class="column-left d-flex ">
+   
+        </div>
+        <div class="column-right d-flex">
+
+        </div>
+      </div>
+    </body>
+
+  );
+}
+
+export default LoginPage;
